305b47fd67f518fa51aea88aac99b30883ae232e
Lý Kiến Phi <53138234+kienphi30999@users.noreply.github.com>
2022-12-07 12:10:50 +0700

    [REMOVE] comment

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -8,7 +8,6 @@ import footer1 from "images/footer1.png";
 const limit_constant = 3;
 
 function paginate(array, limit, page) {
-  // human-readable page numbers usually start with 1, so we reduce 1 in the first argument
   return array.slice((page - 1) * limit, page * limit);
 }
 
